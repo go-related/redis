19848ee16d3ddbfbfa55d96185e68deca5dd9547
juliant <julian.tabaku@outlook.com>
2023-08-09 23:39:59 +0200

    add sample db redis setting value and loading it

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	ctx := context.Background()
+
+	/*	err := client.Set(ctx, "foo", "bar", 0).Err()
+		if err != nil {
+			panic(err)
+		}*/
+
+	val, err := client.Get(ctx, "foo").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("foo", val)
+	fmt.Println("finished executing")
 }
