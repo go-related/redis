a9239c72f6fa5b10736281b2040519fe23ebaae1
juliant <julian.tabaku@outlook.com>
2023-09-08 15:55:39 +0200

    add viper to with both of the services from one config file.Add sample config yaml into repo

--- commands.go
+++ commands.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-related/redis/configurations"
 	"github.com/spf13/cobra"
 )
 
 func createCommands() {
 	var fooCmd = &cobra.Command{
-		Use:   "foo",
-		Short: "Run the foo command",
+		Use:   "service1",
+		Short: "Run the service1 command",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Running foo command")
+
+			fmt.Printf("Running service %s", configurations.ApplicationConfiguration.Service1.Name)
 		},
 	}
 
 	var barCmd = &cobra.Command{
-		Use:   "bar",
-		Short: "Run the bar command",
+		Use:   "service2",
+		Short: "Run the service2 command",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Running bar command")
+			fmt.Printf("Running service %s", configurations.ApplicationConfiguration.Service2.Name)
 		},
 	}
 	rootCmd.AddCommand(fooCmd)
--- configurations/configuration.go
+++ configurations/configuration.go
@@ -1,1 +1,39 @@
 package configurations
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type Configurations struct {
+	Service1 struct {
+		Port string
+		Name string
+	}
+	Service2 struct {
+		Port string
+		Name string
+	}
+}
+
+var ApplicationConfiguration Configurations
+
+func init() {
+	v := viper.New()
+	v.SetConfigName("default")
+	v.SetConfigType("yml")
+	v.AddConfigPath(".")
+	v.AddConfigPath("./config")
+
+	if err := v.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.WithError(err).Errorf("error loading config file")
+		}
+	}
+	v.AutomaticEnv()
+	err := v.UnmarshalExact(&ApplicationConfiguration)
+	if err != nil {
+		log.WithError(err).Errorf("error mapping config file to type")
+	}
+}
