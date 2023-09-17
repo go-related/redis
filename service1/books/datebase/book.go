2061f2783918786fbfbe9509b930aae6c3b238ce
juliant <julian.tabaku@outlook.com>
2023-09-17 21:48:07 +0200

    Implement books in-memory database

--- service1/books/datebase/book.go
+++ service1/books/datebase/book.go
@@ -2,7 +2,9 @@ package datebase
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-related/redis/service1/books/model"
+	"strings"
 )
 
 type BooksDB interface {
@@ -19,10 +21,11 @@ type BooksDB interface {
 	GetAllGenres(ctx context.Context) ([]model.Genre, error)
 	GetGenresById(ctx context.Context, Id uint) (*model.Genre, error)
 
-	CreateBook(ctx context.Context, data model.Book) error
+	CreateBook(ctx context.Context, data model.Book) (model.Book, error)
 	UpdateBook(ctx context.Context, data model.Book) error
 	DeleteBook(ctx context.Context, Id uint) error
 	GetAllBooks(ctx context.Context) ([]model.Book, error)
+	GetBookById(ctx context.Context, Id uint) (*model.Book, error)
 	SearchBooksByTitle(ctx context.Context, title string) ([]model.Book, error)
 }
 
@@ -59,31 +62,60 @@ func NewBooksDB() BooksDB {
 	}
 }
 
-func (b *booksDb) CreateBook(ctx context.Context, data model.Book) error {
-	//TODO implement me
-	panic("implement me")
+func (b *booksDb) CreateBook(ctx context.Context, data model.Book) (model.Book, error) {
+	data.ID = uint(len(b.Books) + 1)
+	data.IsDeleted = false
+	b.Books = append(b.Books, &data)
+	return data, nil
 }
 
 func (b *booksDb) UpdateBook(ctx context.Context, data model.Book) error {
-	//TODO implement me
-	panic("implement me")
+	currentData, err := b.GetBookById(ctx, data.ID)
+	if err != nil {
+		return err
+	}
+	currentData.Title = data.Title
+	currentData.Authors = data.Authors //this is not supposed to be called on a patch
+	currentData.Genres = data.Genres
+	return nil
 }
 
 func (b *booksDb) DeleteBook(ctx context.Context, Id uint) error {
-	//TODO implement me
-	panic("implement me")
+	currentData, err := b.GetBookById(ctx, Id)
+	if err != nil {
+		return err
+	}
+	currentData.IsDeleted = true
+	return nil
 }
 
 func (b *booksDb) GetAllBooks(ctx context.Context) ([]model.Book, error) {
 	var result []model.Book
 
 	for _, data := range b.Books {
-		result = append(result, *data)
+		if !data.IsDeleted {
+			result = append(result, *data)
+		}
 	}
 	return result, nil
 }
 
 func (b *booksDb) SearchBooksByTitle(ctx context.Context, title string) ([]model.Book, error) {
-	//TODO implement me
-	panic("implement me")
+	var result []model.Book
+
+	for _, data := range b.Books {
+		if !data.IsDeleted && strings.Contains(data.Title, title) {
+			result = append(result, *data)
+		}
+	}
+	return result, nil
+}
+
+func (b *booksDb) GetBookById(ctx context.Context, Id uint) (*model.Book, error) {
+	for _, dt := range b.Books {
+		if dt.ID == Id {
+			return dt, nil
+		}
+	}
+	return nil, fmt.Errorf("entity not found")
 }
